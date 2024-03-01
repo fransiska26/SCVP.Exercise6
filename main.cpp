437ad70a6a684b4f411dee15e4dae98bdf03124c
Fransiska <fransiska26@hotmail.com>
2024-03-01 22:24:12 +0100

    single initiator and target (task 1)

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@
 int sc_main (int, char **)
 {
     processor cpu0("cpu0", "stimuli1.txt", sc_time(1, SC_NS));
-
-    // TODO: add your code here
+    memory<1024> memory0("memory");
+    
+    //bind cpu to memory
+    cpu0.iSocket.bind(memory0.tSocket);
 
     std::cout << std::endl << "Name "
               << std::setfill(' ') << std::setw(10)
